a6777850f84207e97413962742f7538d2b7b9338
Andrew Keil <monkeyninja147@gmail.com>
2018-04-26 21:59:30 +0000

    Initial Commit

---
+++ monty.h
@@ -0,0 +1,17 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <ctype.h>
+
+#define BUFSIZE 1024
+#define LIFO 1
+#define FILO 0
+
